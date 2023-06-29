88048739c5d394650dc4e32bcd46cad86b7dcbaa
Victor Lagerfors <victor@lagerfors.com>
2023-06-29 16:05:33 +0200

    fixed build errors

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import "./App.css";
-import { reduxStore, setName } from "./utils/reduxStore";
+import { reduxStore } from "./utils/reduxStore";
 import { LayoutWrapper } from "./layout/LayoutWrapper";
 import { Lists } from "./pages/Lists";
 
